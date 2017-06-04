c90b6c0204a08bdc490817cfdea5754ccb3c84d9
Waseem Ilyas <hello@waseemilyas.uk>
2017-06-04 18:02:24 +0100

    GHB-1: Slider working

--- js/main.js
+++ js/main.js
@@ -1,1 +1,52 @@
 
+$(function initializeSliders(){
+
+	$('.slider').each(function(){
+		var track = $(this).find('.track');
+		var total_banners = track.children().size();
+		$(this).append('<div class="slider-arrow left"><div class="slider-icon left"><i class="fa fa-arrow-circle-left" aria-hidden="true"></i></div></div><div class="slider-arrow right"><div class="slider-icon right"><i class="fa fa-arrow-circle-right" aria-hidden="true"></i></div></div>');
+		if(total_banners > 1) $(this).find('.slider-icon.right').addClass('active');
+		$(this).append('<div class="circles"></div>');
+		var circles = $(this).find('.circles');
+		for (var i = 1; i <= total_banners; i++) {
+			circles.append('<div class="circle"></div>');
+		}
+		circles.find(">:first-child").addClass('active');
+
+		$('.slider-icon').click(function(){
+			if( !$(this).hasClass('active') ) return;
+			var sliding_right, translation, transformation;
+			if( $(this).hasClass('right') ) sliding_right = true;
+			var slider = $(this).closest('.slider');
+			var track = $(slider).find('.track');
+			var circles = $(track).find('.circles');
+			var left_arrow = $(slider).find('.slider-icon.left');
+			var right_arrow = $(slider).find('.slider-icon.right');
+			var total_banners = track.children().size() - 1;
+			var current_position = parseInt( (track.css('transform').split(',')[4] / $(document).width()) / -1);
+			if( !Number.isInteger(current_position) ) current_position = 0;
+			if(current_position > 0) {
+				sliding_right ? translation = (current_position + 1) : translation = (current_position - 1);
+				if( (translation <= total_banners) && (translation >= 0) ) {
+					transformation = translation * (-100);
+					track.css('-webkit-transform', 'translateX(' + transformation + '%)');
+					track.css('transform','translateX(' + transformation + '%)');
+					sliding_right ? left_arrow.addClass('active') : right_arrow.addClass('active');
+					if (translation >= (total_banners)) right_arrow.removeClass('active');
+					if (translation == 0) left_arrow.removeClass('active');
+				}
+			} else {
+				if(total_banners >= 1){
+					track.css('-webkit-transform', 'translateX(-100%)');
+					track.css('transform','translateX(-100%)');
+					left_arrow.addClass('active');
+					if(total_banners <= 1) right_arrow.removeClass('active');
+				} else {
+					right_arrow.removeClass('active');
+				}
+			}
+		});
+
+	});
+
+});
