21348fe9e2cc64922435380270117d1c08839085
Waseem Ilyas <hello@waseemilyas.uk>
2018-03-18 11:43:48 +0000

    Remove Mother's Day and outdated promotions

--- js/main.js
+++ js/main.js
@@ -175,8 +175,6 @@ $(function(){
 	var max_slides = $('.news .slider .track .slide').length;
 
 	var news_slides = [];
-	news_slides.push('promos.jpg');
-	news_slides.push('promos.jpg');
 	news_slides.push('burger.jpg');
 	news_slides.push('steak.jpg');
 	news_slides.push('coffee.jpg');
